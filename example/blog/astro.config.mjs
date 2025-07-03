abe2ccba089bba7b80f7e9b2babc3e4d2511a195
Shedrach Okonofua <shedrachokonofua@gmail.com>
2025-07-03 11:32:56 +0000

    Clean-up blog implementation

--- example/blog/astro.config.mjs
+++ example/blog/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
